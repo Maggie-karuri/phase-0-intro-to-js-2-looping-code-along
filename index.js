6d19070c077668f9280a9371d833a5ab7a0b4c52
Maggie-karuri <karurimargaret111@gmail.com>
2024-03-17 19:11:54 +0300

    countdown

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function countDown() {
+    for (let i = 10; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  countDown();
+  function writeCards(names, eventName) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return thankYouMessages;
+  }
+  module.exports = { writeCards };
